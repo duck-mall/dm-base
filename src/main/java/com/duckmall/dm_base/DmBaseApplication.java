9edbca9b067a96dc27ded83716ab524ce47d8a7a
frank <1823427924@qq.com>
2022-08-17 23:54:11 +0800

    first commit

---
+++ src/main/java/com/duckmall/dm_base/DmBaseApplication.java
@@ -0,0 +1,13 @@
+package com.duckmall.dm_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duckmall/dm_base/DmBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duckmall.dm_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
